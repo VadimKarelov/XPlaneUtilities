1b56951bdc4afbcf19e05de4b4078d449abc4efc
VadimKarelov <vadim.karelov.02@mail.ru>
2022-06-23 17:06:58 +0500

    Completed ErrorsList

---
+++ XPlaneUtilsWPF/Forms/ErrorsWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XPlaneUtilsWPF.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для ErrorsWindow.xaml
+    /// </summary>
+    public partial class ErrorsWindow : Window
+    {
+        public ErrorsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadErrors_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                listBox_ErrorsList.ItemsSource = XPU.GetErrors();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ XPlaneUtilsWPF/Forms/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using XPlaneUtilsWPF.Forms;
+
+namespace XPlaneUtilsWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetPath_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Укажите путь к X-Plane.exe", "Уведомление");
+
+            OpenFileDialog f = new OpenFileDialog();
+            if (f.ShowDialog() == true)
+            {
+                XPU.RootPath = f.FileName.Remove(f.FileName.LastIndexOf("\\"));
+                tb_Path.Text = XPU.RootPath;
+            }
+        }
+
+        private void ShowErrors_Click(object sender, RoutedEventArgs e)
+        {
+            new ErrorsWindow().ShowDialog();
+        }
+    }
+}
